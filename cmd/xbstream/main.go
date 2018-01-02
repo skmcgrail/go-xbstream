d5f4dc01f1c4b88a52bfb0b826d436c0c95bbfb5
Sean McGrail <skmcgrail@gmail.com>
2018-01-01 16:15:24 -0800

    Add GPL Notices to Source Files

--- cmd/xbstream/main.go
+++ cmd/xbstream/main.go
@@ -1,15 +1,35 @@
+/*
+ * Copyright (C) 2017 Sean McGrail
+ * Copyright (c) 2011-2017 Percona LLC and/or its affiliates.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 package main
 
 import (
 	"encoding/binary"
-	"github.com/akamensky/argparse"
-	"github.com/skmcgrail/go-xbstream/xbstream"
 	"hash/crc32"
 	"io"
 	"log"
 	"os"
 	"path/filepath"
 	"sync"
+
+	"github.com/akamensky/argparse"
+	"github.com/skmcgrail/go-xbstream/xbstream"
 )
 
 func init() {
