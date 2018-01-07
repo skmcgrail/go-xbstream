1399ef8a01056df6e38ea8fd9f9a434f5c0f6c62
Sean McGrail <skmcgrail@gmail.com>
2018-01-07 00:01:06 -0800

    Add a basic reader unit-test

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 [![GoDoc](https://godoc.org/github.com/skmcgrail/go-xbstream/xbstream?status.svg)](https://godoc.org/github.com/skmcgrail/go-xbstream/xbstream)
 [![Go Report Card](https://goreportcard.com/badge/github.com/skmcgrail/go-xbstream)](https://goreportcard.com/report/github.com/skmcgrail/go-xbstream)
 
-**This project is still work in progress, though at this time is functional**
-
-Package xbstream provides an Reader and Writer implementation of the [xbstream][0] format.
+xbstream provides an Reader and Writer implementation of the [xbstream][0] archive format.
 
 [0]: https://github.com/percona/percona-xtrabackup/tree/2.3/storage/innobase/xtrabackup
---
+++ xbstream/reader_test.go
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2017 Sean McGrail
+ * Copyright (C) 2011-2017 Percona LLC and/or its affiliates.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package xbstream
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// xbstream archive created by the standard xbstream binary
+// archive contains two files, file1 and file2, that are each 5 bytes in length
+var xbFile = []byte{
+	0x58, 0x42, 0x53, 0x54, 0x43, 0x4b, 0x30, 0x31, 0x00, 0x50, 0x05, 0x00,
+	0x00, 0x00, 0x66, 0x69, 0x6c, 0x65, 0x31, 0x05, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x5d,
+	0xfe, 0x31, 0x4b, 0x87, 0x19, 0x8b, 0xe0, 0x9a, 0x58, 0x42, 0x53, 0x54,
+	0x43, 0x4b, 0x30, 0x31, 0x00, 0x45, 0x05, 0x00, 0x00, 0x00, 0x66, 0x69,
+	0x6c, 0x65, 0x31, 0x58, 0x42, 0x53, 0x54, 0x43, 0x4b, 0x30, 0x31, 0x00,
+	0x50, 0x05, 0x00, 0x00, 0x00, 0x66, 0x69, 0x6c, 0x65, 0x32, 0x05, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x89, 0x58, 0x8b, 0x97, 0x35, 0xbf, 0x06, 0x38, 0x97, 0x58,
+	0x42, 0x53, 0x54, 0x43, 0x4b, 0x30, 0x31, 0x00, 0x45, 0x05, 0x00, 0x00,
+	0x00, 0x66, 0x69, 0x6c, 0x65, 0x32,
+}
+
+func TestNewReader(t *testing.T) {
+	xb := bytes.NewReader(xbFile)
+
+	reader := NewReader(xb)
+
+	var (
+		expected ChunkHeader
+		chunk    *Chunk
+		err      error
+		payload  = make([]byte, 5)
+	)
+
+	// file1 chunk payload
+	expected = ChunkHeader{
+		Magic:     chunkMagic,
+		Flags:     0,
+		Type:      ChunkTypePayload,
+		PathLen:   5,
+		Path:      []byte("file1"),
+		PayLen:    5,
+		PayOffset: 0,
+		Checksum:  uint32(0x4b31fe5d),
+	}
+	chunk, err = reader.Next()
+	require.NoError(t, err, "error reading file1 chunk payload from xbstream")
+	assert.Equal(t, expected, chunk.ChunkHeader)
+	_, err = chunk.Read(payload)
+	require.NoError(t, err, "error occured reading file1 payload contents")
+	assert.Equal(t, []byte{0x87, 0x19, 0x8b, 0xe0, 0x9a}, payload)
+
+	// file1 EOF
+	expected = ChunkHeader{
+		Magic:     chunkMagic,
+		Flags:     0,
+		Type:      ChunkTypeEOF,
+		PathLen:   5,
+		Path:      []byte("file1"),
+		PayLen:    0,
+		PayOffset: 0,
+		Checksum:  0,
+	}
+	chunk, err = reader.Next()
+	require.NoError(t, err, "error reading eof chunk for file1 from xbstream")
+	assert.Equal(t, expected, chunk.ChunkHeader)
+
+	// file2 chunk payload
+	expected = ChunkHeader{
+		Magic:     chunkMagic,
+		Flags:     0,
+		Type:      ChunkTypePayload,
+		PathLen:   5,
+		Path:      []byte("file2"),
+		PayLen:    5,
+		PayOffset: 0,
+		Checksum:  uint32(0x978b5889),
+	}
+	chunk, err = reader.Next()
+	require.NoError(t, err, "error reading file2 chunk payload from xbstream")
+	_, err = chunk.Read(payload)
+	require.NoError(t, err, "error occured reading file1 payload contents")
+	assert.Equal(t, []byte{0x35, 0xbf, 0x06, 0x38, 0x97}, payload)
+
+	// file2 EOF
+	expected = ChunkHeader{
+		Magic:     chunkMagic,
+		Flags:     0,
+		Type:      ChunkTypeEOF,
+		PathLen:   5,
+		Path:      []byte("file2"),
+		PayLen:    0,
+		PayOffset: 0,
+		Checksum:  0,
+	}
+	chunk, err = reader.Next()
+	require.NoError(t, err, "error reading eof chunk for file2 from xbstream")
+	assert.Equal(t, expected, chunk.ChunkHeader)
+}
